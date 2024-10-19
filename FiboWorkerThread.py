ec06b7d49292fbb4526772824b4463232975b1fd
Juanmaperea <juan.coronado@correounivalle.edu.co>
2024-10-19 17:15:53 -0500

    Cambios en Secuencial y adición de Concurrencia.
    Se realizaron cambios mínimos en el documento de secuencial que ya se tenía y se agregó el documento de concurrencia modificado para que cumpliera con las especificaciones propuestas.

--- FiboWorkerThread.py
+++ FiboWorkerThread.py
@@ -1,45 +1,56 @@
-#
-# Este script define un hilo que se encarga de calcular la posicion 'n' en la 
-# serie de Fibonacci.
-#
-# Autor: John Sanabria - john.sanabria@correounivalle.edu.co
-# Fecha: 2023-01-10
-#
+# Importando paquetes necesarios:
 from threading import Thread
 from fibonacci import fibo
 from time import time
 import multiprocessing
 import sys
 
+# Definiendo clase FibWorker, de donde se heredan las características de Thread para la ejecución:
 class FiboWorker(Thread):
-  def __init__(self, n,tid):
+  def __init__(self, n, tid, vec):
     Thread.__init__(self)
     self.n = n
     self.tid = tid
+    self.vec = vec
 
+# Definiendo la actividad a ejecutar (Calcular el Fibonacci):
   def run(self):
-    print(f"[{self.tid}] Fibonacci de {self.n} es {fibo(self.n)}")
+    for z in range(12):
+      self.vec[z] = fibo(self.vec[z])
+      print(f"[{self.tid}] Fibonacci de {self.n} es {self.vec[z]}")
 
+# Definiendo el main:
 def main():
+
+  # Inicializando vector en 33 para 144 cálculos:
   max_fibo = 33
+  tam = 144 # Tamaño del vector (Número de Fibonaccis a calcular)
+  vectorCon = [max_fibo] * tam
   if len(sys.argv) != 1:
     max_fibo = int(sys.argv[1])
+
+  # Inicializando vector con trabajadores:  
   num_cpus = multiprocessing.cpu_count() # CPUs disponibles
-  print(f"Calculando el fibonacci {max_fibo} en {num_cpus} CPUs")
-  hilos = [] # Vector de hilos
+  trabajadores = []
+  print(f"Calculando el fibonacci {max_fibo} en {num_cpus} CPU's")
+
+  # Asignando CPU's y relacionándolas con segmentos del vector:
   ts = time() # se toma tiempo 
   for x in range(num_cpus): # Ciclo para crear trabajadores
     print(f"Trabajador {x} comienza")
-    worker = FiboWorker(max_fibo,x)
+    worker = FiboWorker(max_fibo, x, vectorCon[(x * 12): (x * 12 + 12)])
     worker.start()
-    hilos.append(worker)
+    trabajadores.append(worker)
 
+# Ciclo para esperar a que las CPU's:
   for x in range(num_cpus): # Ciclo para esperar por trabajadores
     print(f"Esperando por trabajador {x}")
-    hilos[x].join()
-
+    trabajadores[x].join()
+    vectorCon[(x * 12): (x * 12 + 12)] = trabajadores[x].vec
+    
+# Imprimiento tiempo y el vector:
   print(f"Tomo {time() - ts}")
-
+  print(f"El vector resultante es: {vectorCon}")
 
 if __name__ == "__main__":
   main()
--- secuencial.py
+++ secuencial.py
@@ -1,15 +1,10 @@
-#
-# Este script calcula de manera secuencial el Fibonacci 
-# serie de Fibonacci.
-#
-# Autor: Juan Manuel Perea - juan.coronado@correounivalle.edu.co
-# Fecha: 2024-10-16
-#
+# Importando paquetes necesarios:
 from fibonacci import fibo
 from time import time
 import multiprocessing
 import sys
 
+# Definiendo el main:
 def main():
 
 	# Inicializando vector en 33 para 144 cálculos:
@@ -19,7 +14,7 @@ def main():
 	num_cpus = multiprocessing.cpu_count()
 	
 	# Calculando los valores Fibonacci y almacenando en el vector:
-	print(f"Calculando el fibonacci de {max_fibo} en {num_cpus} CPU's")
+	print(f"Calculando el fibonacci {max_fibo} en {num_cpus} CPU's")
 	ts = time()
 	for x in range(tam):
 		y = fibo(max_fibo)
