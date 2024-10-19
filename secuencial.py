531ae23e49021654c1190b0e9938f18d1deecd55
Juanmaperea <juan.coronado@correounivalle.edu.co>
2024-10-19 14:58:06 -0500

    Otros ajustes del archivo secuencual.
    Se corrigieron parámetros y se borraron librerías innecesarias.

--- secuencial.py
+++ secuencial.py
@@ -9,26 +9,26 @@ from fibonacci import fibo
 from time import time
 import multiprocessing
 import sys
-import os
 
 def main():
 
 	# Inicializando vector en 33 para 144 cálculos:
 	max_fibo = 33
-	tam = 10 # Tamaño del vector (Número de Fibonaccis calculados).
+	tam = 144 # Tamaño del vector (Número de Fibonaccis a calcular).
 	vectorSeq = [max_fibo] * tam
 	num_cpus = multiprocessing.cpu_count()
 	
 	# Calculando los valores Fibonacci y almacenando en el vector:
-	print(f"Calculando el fibonacci {max_fibo} en {num_cpus} CPUs")
+	print(f"Calculando el fibonacci de {max_fibo} en {num_cpus} CPU's")
 	ts = time()
 	for x in range(tam):
 		y = fibo(max_fibo)
 		vectorSeq[x] = y
 		print(f"El fibonacci de {max_fibo} es: {y}")
 
-	# Imprimiendo tiempo de ejecución:	
+	# Imprimiendo tiempo de ejecución y el vector resultante:	
 	print(f"Tomo {time() - ts}")
+	print(f"El vector resultante es: {vectorSeq}")
 
 if __name__ == "__main__":
   main()
